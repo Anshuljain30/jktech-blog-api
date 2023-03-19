df900adbb191bea11f0561df939b20862c7e1928
Anshul Jain <anshul_jain@live.com>
2023-03-19 10:39:49 +0530

    Added Post entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,30 @@
-export class Post {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false })
+  body: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user: User;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+}
