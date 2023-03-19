32a1bb4cfdf7bccda154cdd13254012ea88ddfba
Anshul Jain <anshul_jain@live.com>
2023-03-19 12:58:23 +0530

    updated update DTO for posts

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,11 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePostDto {
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   body: string;
 }
