bc01627c06300ecb7109fb1a1f7b61457e257a96
Anshul Jain <anshul_jain@live.com>
2023-03-19 11:57:23 +0530

    added database writes and fetches for posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,19 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { User } from '../users/entities/user.entity';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    createPostDto.user = await this.userRepository.findOneBy({
+      id: createPostDto.userId,
+    });
+    if (!createPostDto.user)
+      throw new BadRequestException('User not found with provided UserID.');
+    delete createPostDto.userId;
+    const post = await this.postRepository.save(createPostDto);
+    console.log(post.user);
+    delete post.user;
+    return post;
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll() {
+    return await this.postRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: string) {
+    return await this.postRepository.findOneBy({ id });
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
