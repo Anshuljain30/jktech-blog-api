ddf660c1cd944048d822b2bead9164cb8b5229e9
Anshul Jain <anshul_jain@live.com>
2023-03-19 16:25:01 +0530

    minor fixes

--- src/auth/stratagy/local.stratagy.ts
+++ src/auth/stratagy/local.stratagy.ts
@@ -9,7 +9,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  async validate(username: string, password: string): Promise<any> {
-    return await this.authService.validateCredentials(username, password);
+  async validate(email: string, password: string): Promise<any> {
+    return await this.authService.validateCredentials(email, password);
   }
 }
