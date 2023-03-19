ca05df1be3a66d9c7d3e7d5b57bc3bb9658ebc09
Anshul Jain <anshul_jain@live.com>
2023-03-19 10:56:19 +0530

    added DTO for posts

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString } from '@nestjs/class-validator';
+import { User } from '../../users/entities/user.entity';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  body: string;
+
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+  user: User;
+}
