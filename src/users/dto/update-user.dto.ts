97b64716f88e6c2099d90947078c73cbf26fa6d0
Anshul Jain <anshul_jain@live.com>
2023-03-19 12:56:58 +0530

    updated update DTO to have optional fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,13 +1,21 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   name: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
   @MaxLength(12)
+  @IsOptional()
   password: string;
 }
